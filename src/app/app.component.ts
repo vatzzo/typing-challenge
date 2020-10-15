10a6df9cf8468183ab135a787b711473883e317d
Mateusz Lata <mateuszlata2@gmail.com>
2020-10-15 13:23:08 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { lorem, random } from 'faker';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomText = lorem.word();
+  userText = '';
+  noWords = 1;
+
+  onInput(value: string) {
+    this.userText = value;
+    
+    if(this.randomText === this.userText) {
+      console.log('Now');
+    }
+  }
+
+  clearInput(text1:string, text2:string, event: any) {
+    if(text1 === text2) {
+      setTimeout(() => 
+        { 
+          event.target.value = "";
+          this.userText = '';
+          this.generateSentance();
+        }, 800 );
+    }
+  } 
+
+  getClassName(randomLetter: string, userLetter: string) {
+    if(!userLetter) {
+      return 'underline-black'
+    }
+    
+    if(randomLetter === userLetter) {
+      return 'underline-green';
+    } else {
+      return 'underline-red';
+    }
+  }
+
+  chooseOption(value: any) {
+    value.target.parentElement.children[this.noWords-1].classList.remove('option--active');
+    this.noWords = value.target.id;
+    value.target.classList.add('option--active');
+    this.generateSentance();
+  }
+
+  generateSentance() {
+    this.randomText = '';
+    for(let i = 1; i <= this.noWords; i++) {
+      this.randomText += (lorem.word() + ' ');
+    }
+    this.randomText = this.randomText.trim(); 
+  }
 }
